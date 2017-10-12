89d9b38bd63dcaf2ab021a5b495b7bcac417794b
Hisyam Mukharor <IT040817@BTPN>
2017-10-12 15:26:00 +0700

    create method baru

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -14,26 +14,16 @@ public class Accounts {
 	return sum % 11 == 0;
     }
 
-    public Transfer makeTransfer(String counterAccount, Money amount)
-	    throws BusinessException {
-	// 1. Check withdrawal limit:
-	if (amount.greaterThan(this.transferLimit)) {
-	    throw new BusinessException("Limit exceeded!");
-	}
-	// 2. Assuming result is 9-digit bank account number, validate 11-test:
-	int sum = 0;
-	for (int i = 0; i < counterAccount.length(); i++) {
-	    char character = counterAccount.charAt(i);
+    public static int sumAcc(String cc) {
+
+	int sum = 0; // <1>
+	for (int i = 0; i < cc.length(); i++) {
+	    char character = cc.charAt(i);
 	    int characterValue = Character.getNumericValue(character);
 	    sum = sum + (9 - i) * characterValue;
 	}
-	if (sum % 11 == 0) {
-	    // 3. Look up counter account and make transfer object:
-	    CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-	    Transfer result = new Transfer(this, acct, amount);
-	    return result;
-	} else {
-	    throw new BusinessException("Invalid account number!");
-	}
+
+	return sum;
     }
+
 }
