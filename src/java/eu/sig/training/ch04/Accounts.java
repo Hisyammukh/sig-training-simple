3a55eae1a0d62425f85524c06b2b15d5d541f2c3
Hisyam Mukharor <IT040817@BTPN>
2017-10-12 14:49:43 +0700

    Update Write Codes Once

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -14,6 +14,28 @@ public class Accounts {
 	}
 	return sum % 11 == 0;
     }
-    // end::isValid[]
 
+    // end::isValid[]
+    public Transfer makeTransfer(String counterAccount, Money amount)
+	    throws BusinessException {
+	// 1. Check withdrawal limit:
+	if (amount.greaterThan(this.transferLimit)) {
+	    throw new BusinessException("Limit exceeded!");
+	}
+	// 2. Assuming result is 9-digit bank account number, validate 11-test:
+	int sum = 0;
+	for (int i = 0; i < counterAccount.length(); i++) {
+	    char character = counterAccount.charAt(i);
+	    int characterValue = Character.getNumericValue(character);
+	    sum = sum + (9 - i) * characterValue;
+	}
+	if (sum % 11 == 0) {
+	    // 3. Look up counter account and make transfer object:
+	    CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+	    Transfer result = new Transfer(this, acct, amount);
+	    return result;
+	} else {
+	    throw new BusinessException("Invalid account number!");
+	}
+    }
 }
