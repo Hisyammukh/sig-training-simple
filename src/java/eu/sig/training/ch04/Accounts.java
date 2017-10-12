fcba652a06278f0def4d34f8c8f636f4e48d1a71
Hisyam Mukharor <IT040817@BTPN>
2017-10-12 14:44:36 +0700

    Update Simple Unit of codes

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -3,16 +3,17 @@ package eu.sig.training.ch04;
 public class Accounts {
     @SuppressWarnings("unused")
     public static CheckingAccount findAcctByNumber(String number) {
-        return new CheckingAccount();
+	return new CheckingAccount();
     }
 
     // tag::isValid[]
     public static boolean isValid(String number) {
-        int sum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
-        }
-        return sum % 11 == 0;
+	int sum = 0;
+	for (int i = 0; i < number.length(); i++) {
+	    sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
+	}
+	return sum % 11 == 0;
     }
     // end::isValid[]
+
 }
